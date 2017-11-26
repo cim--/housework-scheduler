a3f4249888649d39a3b701bb24c540200365139f
cim <cim@oolite.org>
2017-11-26 09:35:09 +0000

    Use visibility field

--- database/seeds/TasktypeSeeder.php
+++ database/seeds/TasktypeSeeder.php
@@ -15,13 +15,13 @@ class TasktypeSeeder extends Seeder
     public function run()
     {
         $types = [
-            ['INTERNAL', 7, 'Vacuum ground floor'],
-            ['INTERNAL', 7, 'Vacuum first floor'],
-            ['INTERNAL', 28, 'Clean oven'],
-            ['INTERNAL', 14, 'Clean hob'],
-            ['EXTERNAL', 14, 'Put out recycling'],
-            ['EXTERNAL', 14, 'Put out rubbish'],
-            ['ONCE', null, 'Let electrician in']
+            ['INTERNAL', 7, 'Vacuum ground floor', 0],
+            ['INTERNAL', 7, 'Vacuum first floor', 0],
+            ['INTERNAL', 28, 'Clean oven', 0],
+            ['INTERNAL', 14, 'Clean hob', 0],
+            ['EXTERNAL', 14, 'Put out recycling', 1],
+            ['EXTERNAL', 14, 'Put out rubbish', 1],
+            ['ONCE', null, 'Let electrician in', 0]
         ];
 
         foreach ($types as $info) {
@@ -30,6 +30,7 @@ class TasktypeSeeder extends Seeder
             $type->name = $info[2];
             $type->scheduler_id = $scheduler->id;
             $type->schedule = $info[1];
+            $type->visibility = $info[3];
             $type->save();
         }
     }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -16,24 +16,28 @@
 	</tr>
   </thead>
   <tbody>
-  @foreach ($active as $task)
-  <tr class='task'>
-	<td class='name'>{{$task->tasktype->name}}</td>
-	@if ($task->date->isToday())
-	<td class='date date-current'>{{$task->date->format("j F")}}</td>
-	@elseif ($task->date->isPast())
-	<td class='date date-overdue'>{{$task->date->format("j F")}}</td>
-	@else
-	<td class='date date-future'>{{$task->date->format("j F")}}</td>
-	@endif
-	<td class='completion'>
-	  <form action='{{route('complete', $task->id)}}' method='post'>
-		{{csrf_field()}}
-		<button type='submit'>Done</button>
-	  </form>
-	</td>
-  </tr>
-  @endforeach
+	@foreach ($active as $task)
+	<tr class='task'>
+	  <td class='name'>{{$task->tasktype->name}}</td>
+	  @if ($task->date->isToday())
+	  <td class='date date-current'>{{$task->date->format("j F")}}</td>
+	  @elseif ($task->date->isPast())
+	  <td class='date date-overdue'>{{$task->date->format("j F")}}</td>
+	  @else
+	  <td class='date date-future'>{{$task->date->format("j F")}}</td>
+	  @endif
+	  <td class='completion'>
+		@if ($task->date->isFuture() && ($task->tasktype->visibility != 0 && $task->date->gte(new Carbon\Carbon("+".$task->tasktype->visibility." days"))))
+		-
+		@else
+		<form action='{{route('complete', $task->id)}}' method='post'>
+		  {{csrf_field()}}
+		  <button type='submit'>Done</button>
+		</form>
+		@endif
+	  </td>
+	</tr>
+	@endforeach
   </tbody>
 </table>
 @stop
